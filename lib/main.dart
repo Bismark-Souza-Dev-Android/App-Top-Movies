ec1cdfdb77bb11911e5e54c13a21b3c3c07f1296
Bismark Souza <xkillrr97x@gmail.com>
2024-04-29 21:20:45 -0300

    [FEATURE][MOVIE-1] - inicio consumos de apis

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:first_app/movies_list.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
@@ -15,10 +17,20 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   void _getListApi() {
-    http.get(Uri.https('api.themoviedb.org', '4/list/1'), headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmYzMzA1OGZlYTkwZmU2MmVjMDRjNzY5MzUwZjI5MyIsInN1YiI6IjYzNmQ4MThiMDQ5OWYyMDA3ZmViNTg2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.56OIf4q5ZnPEfXHa_niANV1iz4L3LfcTf5iS3arwbhM'
-    },
-    ).then((Response value) => print(value.body));
+    http.get(
+      Uri.https('api.themoviedb.org', '4/list/1'),
+      headers: {
+        'Authorization':
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmYzMzA1OGZlYTkwZmU2MmVjMDRjNzY5MzUwZjI5MyIsInN1YiI6IjYzNmQ4MThiMDQ5OWYyMDA3ZmViNTg2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.56OIf4q5ZnPEfXHa_niANV1iz4L3LfcTf5iS3arwbhM'
+      },
+    ).then((Response value) {
+      if (value.statusCode == 200) {
+        MoviesList movies = MoviesList.fromJson(
+          jsonDecode(value.body),
+        );
+        print(movies);
+      }
+    });
   }
 
   @override
